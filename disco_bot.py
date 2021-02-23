d258deb88d36af4d5859ef6c3bf18f5771ff8ffb
Dan Graney <dan.graney@eagleview.com>
2021-02-22 21:41:10 -0500

    Ensure we only run check_receipts once per day.

--- disco_bot.py
+++ disco_bot.py
@@ -6,7 +6,9 @@ from datetime import datetime
 from pytz import timezone,UTC
 import re
 from receipt_parser import parse_receipt_messages,get_hot_tickers
+import traceback
 
+reported_dates=[]
 
 tz = timezone('EST')
 load_dotenv()
@@ -26,33 +28,40 @@ async def on_message(message):
     #     await check_receipts()
     pass
 
-@tasks.loop(seconds=5.0)
+@tasks.loop(minutes=5.0)
 async def check_receipts():
-    # Only trigger at 4 pm
-    if datetime.now(tz).hour != 21:
-        print('nah')
-        return
+    try:
+        today_str = datetime.now(tz).strftime("%Y%m%d")
 
-    test_channel = client.get_channel(813526553011027980)
-    start_of_market = datetime.now(tz).replace(hour=9,minute=30)
-    
-    # Get last 200 messages in that channel
-    messages = await test_channel.history(limit=200).filter(
-        lambda m:
-            m.created_at.replace(tzinfo=UTC).astimezone(tz) > start_of_market and
-            m.author != client.user
-    ).flatten()
-    
-    df = parse_receipt_messages(messages)
+        if today_str in reported_dates: return # We already reported today.
+        # Only trigger at 4 pm
+        if datetime.now(tz).hour != 16:
+            print("Not triggering receipt check... Waiting on market close! Current time: ",datetime.now(tz))
+            return
 
-    # Analyze Data
-    hot_tickers = get_hot_tickers(df)
-    hot_ticker_str = f"```{hot_tickers.to_string(index=False)}```"
+        test_channel = client.get_channel(813526553011027980)
+        start_of_market = datetime.now(tz).replace(hour=9,minute=30)
+        
+        # Get last 200 messages in that channel
+        messages = await test_channel.history(limit=200).filter(
+            lambda m:
+                m.created_at.replace(tzinfo=UTC).astimezone(tz) > start_of_market and
+                m.author != client.user
+        ).flatten()
+        
+        df = parse_receipt_messages(messages)
 
-    await test_channel.send(":fire:**TODAY'S HOT TICKERS**:fire:")
-    await test_channel.send(hot_ticker_str)
+        # Analyze Data
+        hot_tickers = get_hot_tickers(df)
+        hot_ticker_str = f"```{hot_tickers.to_string(index=False)}```"
 
-    return messages
+        await test_channel.send(":fire:**TODAY'S HOT TICKERS**:fire:")
+        await test_channel.send(hot_ticker_str)
+
+        reported_dates.append(today_str)
+    except Exception as exc:
+        traceback.print_exc()
+
 
 # @check_receipts.before_loop
 # async def before_receipts():
